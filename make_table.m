f7e23d8ddf7457bee0e5479bde0130cf6a6b2984
pnv4 <pnv4@cornell.edu>
2016-11-16 01:15:12 -0500

    Update make_table.m

--- make_table.m
+++ make_table.m
@@ -4,7 +4,7 @@
 clear; clc;
 % Step 1: Preprocessing
 load('/Users/PHUC_VO/Documents/ECE2200/FinalProject/sample.mat');
-X = y(:,1);                     % use the first channel 
+X = y(:,1);                 % use the first channel 
 resampledSong = resample(X,8000,44100);     % Resample the signal at 8000Hz
 
 % Step 2: Spectrogram 
@@ -30,8 +30,9 @@ log_S = log10(abs(S)+1);
 % Step 3: Feature Extraction (Spectrogram Local Peaks)
 % log_S = [1 1 1 3;1 10 1 2;1 3 1 5;2 11 1 2];
 localPeak = ones(size(log_S,1),size(log_S,2));  
-for i = -4:4        % check magnitude with neighbor in the 9x9 grid (gs=9)
-    for j = -4:4
+gs = 9;
+for i = -(gs-1)/2:(gs-1)/2          % check magnitude with neighbor 
+    for j = -(gs-1)/2:(gs-1)/2      % in the 9x9 grid (gs=9)
         if (i~=0 || j~=0)
             CS = circshift(log_S,[j,i]);
             localPeak = and(localPeak,((log_S - CS)>0));
